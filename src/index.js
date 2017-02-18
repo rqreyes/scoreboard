76a73505d3fe307353bd1e2715960fe825695b6f
Randy Reyes <rqreyes@gmail.com>
2017-02-18 11:10:02 -0800

    Add components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,99 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
+
+var PLAYERS = [
+	{
+		name: 'Know Idea',
+		score: 31,
+		id: 1,
+	},
+	{
+		name: 'Golden Glitch',
+		score: 28,
+		id: 2,
+	},
+	{
+		name: 'Code Bene',
+		score: 33,
+		id: 3,
+	},
+	{
+		name: 'Project Mayhem',
+		score: 30,
+		id: 4,
+	},
+];
+
+function Header(props) {
+	return (
+		<div className='header'>
+			<h1>{props.title}</h1>
+		</div>
+	)
+}
+
+Header.propTypes = {
+	title: React.PropTypes.string.isRequired,
+};
+
+function Counter(props) {
+	return (
+		<div className='counter'>
+			<button className='counter-action decrement'> - </button>
+			<div className='counter-score'> {props.score} </div>
+			<button className='counter-action increment'> + </button>
+		</div>
+	)
+}
+
+Counter.propTypes = {
+	score: React.PropTypes.number.isRequired,
+}
+
+function Player(props) {
+	return (
+		<div className='player'>
+			<div className='player-name'>
+				{props.name}
+			</div>
+			<div className='player-score'>
+				<Counter score={props.score} />
+			</div>
+		</div>
+	)
+}
+
+Player.propTypes = {
+	name: React.PropTypes.string.isRequired,
+	score: React.PropTypes.number.isRequired,
+}
+
+function Application(props) {
+	return (
+		<div className='scoreboard'>
+			<Header title={props.title} />
+
+			<div className='players'>
+				{props.players.map(function(player) {
+					return <Player name={player.name} score={player.score} key={player.id} />
+				})}
+			</div>
+		</div>
+	);
+}
+
+Application.propTypes = {
+	title: React.PropTypes.string,
+	players: React.PropTypes.arrayOf(React.PropTypes.shape({
+		name: React.PropTypes.string.isRequired,
+		score: React.PropTypes.number.isRequired,
+		id: React.PropTypes.number.isRequired,
+	})).isRequired,
+};
+
+Application.defaultProps = {
+	title: 'Scoreboard',
+}
+
+ReactDOM.render(<Application players={PLAYERS} />, document.querySelector('#container'));
